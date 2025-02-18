265ecaf91971edb29139e91631e13c11aae3c418
seungju_hong <hongsj430@gmail.com>
2025-02-18 23:50:40 +0900

    add: style - display name init modal

--- src/component/modal/DisplayNameModal.jsx
+++ src/component/modal/DisplayNameModal.jsx
@@ -2,11 +2,12 @@ import { useEffect, useRef, useState } from "react";
 import evt from "@/utils/event-handler";
 import { checkDisplayName, initDisplayName } from "@/api/nakama";
 import { FaCheck } from "react-icons/fa";
-
+import { MdEdit } from "react-icons/md";
 import { CgSpinner } from "react-icons/cg";
 import { BsThreeDots } from "react-icons/bs";
 
 import { useNakama } from "@/providers/NakamaProvider";
+import { WSButton, WSModal, WSModalHeader } from "../ui/WSComponents";
 const DisplayNameModal = () => {
   const { refreshAccount } = useNakama();
   const checkTimeout = useRef(null);
@@ -52,51 +53,51 @@ const DisplayNameModal = () => {
   }, [displayName]);
   if (!isOpen) return null;
   return (
-    <div
-      className="absolute top-0 left-0 z-20 w-full h-full bg-black/50 flex flex-col gap-2 items-center justify-center"
-      onClick={() => setOpen(false)}
-    >
-      <div
-        className="bg-white border-2 border-[var(--color-chocolate-900)] rounded-2xl"
-        onClick={(e) => e.stopPropagation()}
-      >
-        <div className="text-xl p-1 font-bold text-center border-b-2 border-[var(--color-chocolate-900)] text-[var(--color-chocolate-900)]">
-          SET NICKNAME
-        </div>
-        <div className="flex flex-col gap-2 p-2">
-          <div className="flex gap-2 items-center">
-            <input
-              value={displayName}
-              onChange={handleChange}
-              type="text"
-              className="p-2 font-bold  bg-gray-200 rounded-2xl"
-            />
+    <WSModal onClick={() => setOpen(false)}>
+      <div onClick={(e) => e.stopPropagation()}>
+        <WSModalHeader className="flex w-full p-2">
+          <div className="text-xl font-bold">SET NICKNAME</div>
+        </WSModalHeader>
+        <div className="flex flex-col gap-1 p-4">
+          <div className="text-[var(--color-chocolate-900)] font-bold">
+            NICKNAME
           </div>
-          {displayName.length > 0 ? (
-            <button
-              onClick={submit}
-              className={`flex items-center justify-center p-2 rounded-2xl cursor-pointer ${
-                checking ? "" : checked ? "bg-green-400" : "bg-red-400"
-              }`}
-            >
-              {checking ? (
-                <div className="text-2xl">
-                  <CgSpinner className="animate-spin" />
-                </div>
-              ) : (
-                <div className={`text-2xl  text-white`}>
-                  <FaCheck />
-                </div>
-              )}
-            </button>
-          ) : (
-            <div className="flex items-center justify-center p-2 rounded-2xl   text-2xl">
-              <BsThreeDots />
+          <div className="flex gap-2">
+            <div className="flex-1 relative flex gap-2 items-center">
+              <input
+                type="text"
+                value={displayName}
+                onChange={handleChange}
+                className="w-full p-2 border-2 border-[var(--color-chocolate-900)] rounded-xl focus:border-[var(--color-chocolate-500)] outline-none text-[var(--color-chocolate-900)] font-bold"
+              />
+              <div className="absolute right-0 top-1/2 -translate-y-1/2 w-10 h-10 flex items-center justify-center">
+                {displayName.length > 0 ? (
+                  checking ? (
+                    <CgSpinner className="animate-spin text-2xl text-chocolate-900" />
+                  ) : (
+                    <FaCheck
+                      className={`text-2xl ${
+                        checked ? "text-green-500" : "text-red-500"
+                      }`}
+                    />
+                  )
+                ) : (
+                  <BsThreeDots className="text-2xl text-chocolate-900" />
+                )}
+              </div>
             </div>
-          )}
+            <WSButton
+              onClick={submit}
+              disabled={
+                checking || !checked || !displayName || displayName.length === 0
+              }
+            >
+              <MdEdit />
+            </WSButton>
+          </div>
         </div>
       </div>
-    </div>
+    </WSModal>
   );
 };
 
